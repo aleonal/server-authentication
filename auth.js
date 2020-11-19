15ae9b2f23c7987ec317fc70204a9d4017916091
Antoine <thelifeofantoine@outlook.com>
2020-11-18 21:11:46 -0700

    Separated HTML and JS from signin.php

---
+++ auth.js
@@ -0,0 +1,27 @@
+function validateUsername(field) {
+    if (field == "") return "No username was entered.\n";
+    else if (field.length < 6)
+        return "Username must be at least 6 characters.\n";
+    else if (/[^a-zA-Z0-9_-]/.test(field))
+        return "Username contains invalid characters.\n";
+    return ""
+}
+function validatePassword(field) {
+    if (field == "") return "No passsword was entered.\n";
+    else if (field.length < 6)
+        return "Password must be at least 6 characters.\n";
+    else if (!/[a-z]/.test(field) || !/[A-Z]/.test(field) || !/[0-9]/.test(field))
+        return "Passwords require at least 1 lowercase letter, uppercase letter, and number.\n";
+    return ""
+}
+function validate(form) {
+    fail = validateUsername(form.username.value)
+    fail += validatePassword(form.password.value)
+    if (fail == "") return true
+    else {
+        alert(fail);
+        form.password.
+        form.password.value = ""
+        return false
+    }
+}
