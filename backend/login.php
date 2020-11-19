3845964d9df063f585ac479928fa34b9fb15993d
Antoine <thelifeofantoine@outlook.com>
2020-11-19 05:27:40 -0700

    Added some comments noting I understand this is not the correct way to do it.

--- backend/login.php
+++ backend/login.php
@@ -1,5 +1,6 @@
 <?php
-    // salt
+    // salt. Note that in a real application, these would be loaded via user input and a
+    // salt like this would be handled differently.
     define('S', 'alk2wfb1fjkaOzhWGbjnhlLNT@$YFKLGshJKGubv2');
     $hn = "localhost";
     $db = "assignment";
