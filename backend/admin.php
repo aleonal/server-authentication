0810008fc12fcc84d5c6d2a7acb5552855ffd7b3
Antoine <thelifeofantoine@outlook.com>
2020-11-30 02:07:15 -0700

    Created template to display user info in admin page. Added backend function to get user data to display in admin page.

--- backend/admin.php
+++ backend/admin.php
@@ -4,8 +4,37 @@
     if ($status < 2) {
         header("Location: ../backend/mainpage.php?error=20", true, 301);
     }else {
-        include("../frontend/admin.html");
+        $user_data = get_user_data();
+        // include("../frontend/admin.html");
     }
 
     exit;
+
+    function get_user_data() {
+        include 'login.php';
+        $users = array();
+
+        
+        $connection = new mysqli($hn, $un, $pw, $db);
+        if ($conn->connection_error) return "Fatal error attempting to connect to database.<br>";
+
+        $q = "SELECT * FROM users";
+        $result = $connection->query($q);
+        if (!$result) die("Error fetching users.");
+
+        $connection->close();
+        $rows = $result->num_rows;
+
+        for ($i = 0; $i < $rows; ++$i) {
+            $user = $result->fetch_array(MYSQLI_ASSOC);
+            unset($user['password']);
+            array_push($users, $user);
+        }
+        $result->close();
+        return $users;
+    }
+
+    function insert_user() {
+        
+    }
 ?>
